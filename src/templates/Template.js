25475ac8f76570fbd8a1053d630fe1e9e8a64bb8
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-08-26 14:52:27 +0200

    lets see now

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,10 +1,10 @@
 import React from "react"
-// import Header from "../components/Header.js"
+import Header from "../components/Header.js"
 import Footer from "../components/Footer.js"
 
 export default function Template({children}) {
   return <>
-  {/* <Header/> */}
+  <Header/>
   <>
   {children}
   </>
